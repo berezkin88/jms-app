2a1bce3060afd39586db1b7e32ac0cc99c36d239
berezkin88 <berezkin05@gmail.com>
2020-09-08 23:47:52 +0300

    add Message POJO

---
+++ src/main/java/person/birch/jmsapp/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package person.birch.jmsapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 1238985517688575453L;
+
+    private UUID id;
+    private String message;
+}
