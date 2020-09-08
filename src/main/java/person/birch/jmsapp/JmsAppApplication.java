27cf449a6f964f26b6d17ce4fe7ef70d58cbe199
berezkin88 <berezkin05@gmail.com>
2020-09-08 23:43:14 +0300

    initial commit

---
+++ src/main/java/person/birch/jmsapp/JmsAppApplication.java
@@ -0,0 +1,13 @@
+package person.birch.jmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsAppApplication.class, args);
+    }
+
+}
