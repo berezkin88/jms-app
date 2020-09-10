79e92fd67c3db647f795d8eccbf86006697a614d
berezkin88 <berezkin05@gmail.com>
2020-09-10 22:53:44 +0300

    connect to docker image

--- src/main/java/person/birch/jmsapp/JmsAppApplication.java
+++ src/main/java/person/birch/jmsapp/JmsAppApplication.java
@@ -1,8 +1,5 @@
 package person.birch.jmsapp;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,13 +9,13 @@ public class JmsAppApplication {
 
     public static void main(String[] args) throws Exception {
 
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-            .setPersistenceEnabled(false)
-            .setJournalDirectory("target/data/journal")
-            .setSecurityEnabled(false)
-            .addAcceptorConfiguration("ivm", "vm://0"));
-
-        server.start();
+//        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//            .setPersistenceEnabled(false)
+//            .setJournalDirectory("target/data/journal")
+//            .setSecurityEnabled(false)
+//            .addAcceptorConfiguration("ivm", "vm://0"));
+//
+//        server.start();
 
         SpringApplication.run(JmsAppApplication.class, args);
     }
