31cf7c7649b0fc81169b5dd7f9202fa26fa893fc
berezkin88 <berezkin05@gmail.com>
2020-09-10 21:56:09 +0300

    set up message server

--- src/main/java/person/birch/jmsapp/JmsAppApplication.java
+++ src/main/java/person/birch/jmsapp/JmsAppApplication.java
@@ -1,12 +1,25 @@
 package person.birch.jmsapp;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsAppApplication {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
+
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+            .setPersistenceEnabled(false)
+            .setJournalDirectory("target/data/journal")
+            .setSecurityEnabled(false)
+            .addAcceptorConfiguration("ivm", "vm://0"));
+
+        server.start();
+
         SpringApplication.run(JmsAppApplication.class, args);
     }
 
